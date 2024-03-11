ec2456ed58095b23483d3ef629b64b25ace96c21
IAm2XTr33M <91885904+IAm2XTr33M@users.noreply.github.com>
2024-03-11 21:25:16 +0100

    looking sexi

--- Scripts/homeScript.js
+++ Scripts/homeScript.js
@@ -1,13 +1,23 @@
-
-
-document.addEventListener('DOMContentLoaded', () => {
-
-});
-
-addEventListener("mousemove", (event) => {
-    
-});
-
-function ExtendBottomLine(){
+async function ExtendBottomLine(){
     document.getElementById("mainDiv").classList.add("mainDivActive");
+    await delay(600);
+
+    openNewTab("portfolio.html");
 }
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function openNewTab(url) {
+    var link = document.createElement('a');
+    link.href = url;
+
+    var clickEvent = new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      view: window
+    });
+
+    link.dispatchEvent(clickEvent);
+  }
--- Scripts/myScript.js
+++ Scripts/myScript.js
@@ -1,18 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const letters = document.querySelectorAll('.letter');
+    const letters = document.querySelectorAll('.letter');
 
-  function lightUpLetters(index) {
-    letters[index].classList.add('letterLight');
-    setTimeout(() => {
-      const nextIndex = (index + 1);
-      if(index < letters.length){
-          lightUpLetters(nextIndex);
-      }
-    }, 200);
-  }
+    function lightUpLetters(index) {
+        if(letters[index] != null){
+            letters[index].classList.add('letterLight');
+            setTimeout(() => {
+            const nextIndex = (index + 1);
+            if(index < letters.length){
+                lightUpLetters(nextIndex);
+            }
+            }, 200);
+        }
+    }
 
-  lightUpLetters(0);
+    lightUpLetters(0);
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    invertOff();
+    async function invertOff(){
+        await delay(3000);
+        document.body.classList.remove("inverted");
+    }
+});
 
-
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
